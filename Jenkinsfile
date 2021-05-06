2031cd54b18cdb18248168ecdad8cca465de4f21
leefowler <fowler.lee8@gmail.com>
2021-05-06 13:39:37 +0200

    added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage("init") {
+            steps {
+                script {
+                  echo "init run" 
+                }
+            }
+        }
+        stage("build") {
+            steps {
+                script {
+                    echo "build run"
+                }
+            }
+        }
+        stage("test") {
+            
+            steps {
+                script {
+                   echo "test run"
+                }
+            }
+        }
+        stage("deploy") {
+            steps {
+                script {
+                    echo "deploy run"
+                }
+            }
+        }
+    }   
+}
--- src/main/java/com/lee/callcenter/IncomingCall.java
+++ src/main/java/com/lee/callcenter/IncomingCall.java
@@ -3,44 +3,26 @@ package com.lee.callcenter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-import java.util.Scanner;
-
-public class IncomingCall  {
+public class IncomingCall {
 
     public static class RingingPhone implements Runnable {
 
         private static final Logger logger = LoggerFactory.getLogger(RingingPhone.class);
 
-        private Call call;
-        private Employee employee;
-
         @Override
         public void run() {
-
-
-            }
-
+            System.out.println("main thread is running.");
         }
 
-        public static void main (String []args){
-
-            int userDecision = 0;
-            Scanner scanner;
-
-                System.out.println("Hello customer, do you want to call our agents? Then press 1. Or press 2 to exit.");
-                scanner = new Scanner(System.in);
-                userDecision = scanner.nextInt();
-
-                if( userDecision == 2){
-                    System.exit(0);
-                }
-
-            //make multiple threads in an array
-            Thread  ringingPhone = new Thread (new RingingPhone());
-            ringingPhone.start();
-
-        }
     }
+
+    public static void main(String[] args) {
+
+        //make multiple threads in an array
+        Thread ringingPhone = new Thread(new RingingPhone());
+        ringingPhone.start();
+
+    }
+}
 
 
